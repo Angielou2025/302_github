ba3ed5072cb5a681f063d6e4f883a6788baeaf54
Angielou <Angielou2024@gmail.com>
2025-08-07 12:34:51 +0800

    Add files via upload

---
+++ Act1.dart
@@ -0,0 +1,7 @@
+void main(){
+ 
+  String name = "Alice"; //Gumawa po ako ng string na variable which is po yung name at value ay Alice 
+  dynamic name1 = "Alice";//Gumawa po ako another Variable para ma asign ko yung number 25 at gumamit ako ng dynamic para e cast yung int into string
+  name1 = 25;//Ito po yung bago ko pong variables bali INT siya at ang value niya ay 25
+  
+  print("Welcome,$name" + " " + "Welcome,$name1");//Dito naman po Gumawa ako ng text Message para i welcome si Alice nag lagay lang ako ng Text na Welcome at tinawag ko yung name ng Variables na nag lalaman ng Alice ,at pinagsama ko lahat ng text ko sa isang message lang bali pinag concatenate ko din yung space,Welcome at yung panibago kong Variable which is yong name1.
